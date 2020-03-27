ded3880434d57f20cac2dbeff8eede3f153a7b19
vb <bonhomme.vincent@gmail.com>
2020-03-27 09:58:30 +0100

    + wings

--- R/datasets.R
+++ R/datasets.R
@@ -1,27 +1,30 @@
 #' @importFrom Momocs2 mosaic
 NULL
 
-#' Outlines: Various shapes
+# outlines ------------------------------------------------
+#' Outlines coordinates of various shapes
 #'
 #' @docType data
 #' @name shapes
 #' @family outlines
-#' @format A [Momocs2::coo_tbl] object with outline coordinates of some various shapes.
+#' @format A [Momocs2::coo_tbl] with:
+#'  * `coo` (`coo_list`): shape coordinates
 #' @source Borrowed default shapes from (c) Adobe Photoshop. Please do not send me to jail.
 #' @examples
 #' Momocs2::mosaic(shapes)
 NULL
 
-#' Curves: Olive stones
+# curves --------------------------------------------------
+#' Curves coordinates of olive stones
 #'
 #' @docType data
 #' @name olea
 #' @family curves
-#' @format A [Momocs2::coo_tbl] object with olive stones fertile half curves coordinates.
-#'  * `coo`: `coo_list` olive stones, fertile halves, coordinates of two orthogonal views
-#'  * `status`: `factor` domesticated or wild
-#'  * `accession` : `factor` variety for domesticated, station for wild
-#'  *  `view` : `factor` VL (lateral) or VD (dorsal)
+#' @format A [Momocs2::coo_tbl] with:
+#'  * `coo` (`coo_list`): olive stones, fertile halves, coordinates of two orthogonal views
+#'  * `status` (`factor`): domesticated or wild
+#'  * `accession` (`factor`): variety for domesticated, station for wild
+#'  *  `view` (`factor`): VL (lateral) or VD (dorsal)
 #' @section Thanks to:
 #' Jean-Frédéric Terral & Sarah Ivorra - UMR ISEM, Montpellier, France.
 #' @references
@@ -34,3 +37,21 @@ NULL
 #' @examples
 #' Momocs2::mosaic(olea)
 NULL
+
+# landmarks -----------------------------------------------
+#' Landmarks coordinates of mosquito wings
+#'
+#' @docType data
+#' @name wings
+#' @family landmarks
+#' @format A [Momocs2::coo_tbl] with:
+#'  * `coo` (`coo_list`):coordinates
+#'  * `group` (`factor`): grouping factor
+#' @references
+#' Rohlf, F. J. and Slice, D. (1990)
+#' Extensions of the Procrustes method for the optimal superimposition of landmarks.
+#' *Systematic Zoology* **39**:1-40.
+#' [doi:10.2307/2992207](https://doi.org/10.2307/2992207)
+#' @family datasets
+NULL
+
